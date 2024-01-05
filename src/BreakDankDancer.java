01544a63004fc8a4a2ee8ec636b33b029ad97a75
gemansipator <gemansipator@gmail.com>
2024-01-05 12:18:26 +0300

    //Пример полиморфизма в Java и использования объектов в программе:
            Dancer dancer = new Dancer("Anton", 18);
    
            Dancer breakDanceDancer = new BreakDankDancer("Alex", 19); // восходящее преобразование к базовому типу
            Dancer electricBoogieDancer = new ElectricBoogieDancer("Igor", 20); // восходящее преобразование к базовому типу
    
            List<Dancer> discotheque = Arrays.asList(dancer,breakDanceDancer,electricBoogieDancer);
            for (Dancer d : discotheque){
                d.dance();// полиморфный вызов метода
            }

---
+++ src/BreakDankDancer.java
@@ -0,0 +1,10 @@
+public class BreakDankDancer extends Dancer{
+    public BreakDankDancer(String name, int age) {
+        super(name, age);
+    }
+    // переопределение метода базового класса
+    @Override
+    public void dance(){
+        System.out.println(toString() + "Я танцую брейк-данс!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Пример полиморфизма в Java и использования объектов в программе:
+
+        Dancer dancer = new Dancer("Anton", 18);
+
+        Dancer breakDanceDancer = new BreakDankDancer("Alex", 19); // восходящее преобразование к базовому типу
+        Dancer electricBoogieDancer = new ElectricBoogieDancer("Igor", 20); // восходящее преобразование к базовому типу
+
+        List<Dancer> discotheque = Arrays.asList(dancer,breakDanceDancer,electricBoogieDancer);
+        for (Dancer d : discotheque){
+            d.dance();// полиморфный вызов метода
+        }
+
     }
 }
